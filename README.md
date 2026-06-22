f4be1471d1471d376f72f52b43327d71628ce07b
Mohanan Kottilingel <VARNA.MOHANANKOTTILINGEL@KINGFISHER.COM>
2026-06-22 23:34:45 +0530

    Milestone 4 completed, created scaffold for framework generation

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 "# direktAI" 
+#venv\Scripts\activate  
+#pip install -r requirements.txt
+#uvicorn main:app --reload
+#http://localhost:8000/docs
--- direcktai-backend/app/services/workspace_service.py
+++ direcktai-backend/app/services/workspace_service.py
@@ -4,17 +4,18 @@ from app.models.documents.workspace_document import WorkspaceDocument
 from app.repositories.workspace_repository import WorkspaceRepository
 from app.services.repository_service import RepositoryService
 from app.services.repository_validator_service import RepositoryValidatorService
+from app.services.framework_bootstrap_service import FrameworkBootstrapService
 
 class WorkspaceService:
     def __init__(self, database):
         self.workspace_repository = (WorkspaceRepository(database))
         self.repository_service = (RepositoryService())
         self.repository_validator = (RepositoryValidatorService())
+        self.framework_bootstrap_service = (FrameworkBootstrapService())
 
     async def create_workspace(self, application_url: str, repository_url :str):
         is_valid = (self.repository_validator.validate_repository_accessible(repository_url))
     
-
         if not is_valid:
             raise Exception("Repository is not accessible")
 
@@ -31,6 +32,11 @@ class WorkspaceService:
         self.repository_service.clone_repository(
             repository_url,
             local_repo_path
+        )
+        self.framework_bootstrap_service.bootstrap(
+            local_repo_path,
+            workspace_id,
+            application_url
         )
 
         document = WorkspaceDocument(
